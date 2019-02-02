5a22b6d301248046c7f366e73f5957e5194edb5c
Ari Sutanto <arisuttanto@gmail.com>
2019-02-02 14:14:32 +0700

    warmup-1 intMax

---
+++ Java/warmup-1/warmup-1 intMax.java
@@ -0,0 +1,10 @@
+/*
+Given three int values, a b c, return the largest.
+*/
+
+public int intMax(int a, int b, int c) {
+    int max = a;
+    if (max < b ) max = b;
+    if (max < c) max = c;
+    return max;
+}
