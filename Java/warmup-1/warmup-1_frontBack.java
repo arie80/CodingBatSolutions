80f96510cf1f36f15817f16cadcebbcab919d901
Ari Sutanto <arisuttanto@gmail.com>
2019-02-02 13:35:17 +0700

    warmup-1 frontBack

---
+++ Java/warmup-1/warmup-1_frontBack.java
@@ -0,0 +1,11 @@
+/*
+Given a string, return a new string where the first and last chars have been exchanged.
+*/
+
+public String frontBack(String str) {
+    if (str.length() > 1) {
+        String mid = str.substring(1, str.length() -1);
+        return str.charAt(str.length() - 1) + mid + str.charAt(0);
+    }
+    return str;
+}
