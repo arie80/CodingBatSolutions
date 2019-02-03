d59e782e319e51b1229f93be7ebd7be4f9cadeb4
Ari Sutanto <arisuttanto@gmail.com>
2019-02-03 14:01:49 +0700

    array-1 commondEnd

--- Java/array-1/array-1.java
+++ Java/array-1/array-1.java
@@ -16,3 +16,23 @@ Given an array of ints, return true if the array is length 1 or more, and the fi
 public boolean sameFirstLast(int[] nums) {
     return nums.length >= 1 && (nums[0] == nums[nums.length - 1]);
 }
+
+/*
+makePi
+Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+*/
+
+public int[] makePi() {
+    int[] i = {3, 1, 4};
+    return i;
+}
+
+/*
+commondEnd
+Given 2 arrays of ints, a and b, return true if they have the same first element or they have the same last element.
+Both arrays will be length 1 or more.
+*/
+
+public boolean commonEnd(int[] a, int[] b) {
+    return a[0] == b[0] || a[a.length - 1] == b[b.length - 1];
+}
