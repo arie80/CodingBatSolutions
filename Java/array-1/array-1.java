06cd04117feec9ba18b2b324923297d7f9d01b30
Ari Sutanto <arisuttanto@gmail.com>
2019-02-03 14:18:29 +0700

    array-1 makeLast

--- Java/array-1/array-1.java
+++ Java/array-1/array-1.java
@@ -137,3 +137,16 @@ Given an int array length 2, return true if it does not contain a 2 or 3.
 public boolean no23(int[] nums) {
     return (nums[0] != 2 && nums[1] != 2) && (nums[0] != 3 && nums[1] != 3);
 }
+
+/*
+makeLast
+Given an int array, return a new array with double the length where its last element is the same as the original array,
+and all the other elements are 0. The original array will be length 1 or more.
+Note: by default, a new int array contains all 0's.
+*/
+
+public int[] makeLast(int[] nums) {
+    int[] arr = new int[nums.length * 2];
+    arr[arr.length -1] = nums[nums.length -1];
+    return arr;
+}
