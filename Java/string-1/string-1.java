9fad2431cfb8bbb277bc613c44567bfbb1f40f1f
Ari Sutanto <arisuttanto@gmail.com>
2019-02-03 12:06:32 +0700

    string-1 extraEnd

--- Java/string-1/string-1.java
+++ Java/string-1/string-1.java
@@ -36,3 +36,14 @@ e.g. "<<word>>". Note: use str.substring(i, j) to extract the String starting at
 public String makeOutWord(String out, String word) {
     return out.substring(0, 2) + word + out.substring(out.length() -2, out.length());
 }
+
+/*
+extraEnd
+Given a string, return a new string made of 3 copies of the last 2 chars of the original string.
+The string length will be at least 2.
+*/
+
+public String extraEnd(String str) {
+    String last = str.substring(str.length() - 2);
+    return last + last + last;
+}
