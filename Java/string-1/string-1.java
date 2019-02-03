82e49e360546dadf01d5a4789ba1c4ca5aaee2a9
Ari Sutanto <arisuttanto@gmail.com>
2019-02-03 12:27:54 +0700

    string-1 nTwice

--- Java/string-1/string-1.java
+++ Java/string-1/string-1.java
@@ -170,3 +170,13 @@ public boolean endsLy(String str) {
     if (str.length() <= 1) return false;
     return (str.substring(str.length() - 2).equals("ly"));
 }
+
+/*
+nTwice
+Given a string and an int n, return a string made of the first and last n chars from the string.
+The string length will be at least n.
+*/
+
+public String nTwice(String str, int n) {
+    return str.substring(0, n) + str.substring(str.length() - n);
+}
