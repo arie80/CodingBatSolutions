d5f7066609725b5b1fc29caeb4172437bd06533a
Ari Sutanto <arisuttanto@gmail.com>
2019-02-03 11:10:20 +0700

    warmup-2 last2

--- Java/warmup-2/warmup-2.java
+++ Java/warmup-2/warmup-2.java
@@ -78,7 +78,21 @@ Given a non-empty string like "Code" return a string like "CCoCodCode".
 public String stringSplosion(String str) {
     String result = "";
     for (int i = 0; i < str.length(); i++) {
-        result += str.substring(0, i + 1 );
+        result += str.substring(0, i + 1);
     }
     return result;
+}
+
+/*
+last2
+Given a string, return the count of the number of times that a substring length 2 appears in the string
+and also as the last 2 chars of the string, so "hixxxhi" yields 1 (we won't count the end substring).
+*/
+
+public int last2(String str) {
+    int count = 0;
+    for (int i = 0; i < str.length() - 2; i++) {
+        if (str.substring(i, i + 2).equals(str.substring(str.length() - 2))) count++;
+    }
+    return count;
 }
