81359ef44bcda7b5c70a86246e7134dc0f2e3f58
Ari Sutanto <arisuttanto@gmail.com>
2019-02-03 13:33:32 +0700

    string-1 minCat

--- Java/string-1/string-1.java
+++ Java/string-1/string-1.java
@@ -295,3 +295,17 @@ public boolean frontAgain(String str) {
     String front = str.substring(0, 2);
     return front.equals(str.substring(str.length() - 2));
 }
+
+/*
+minCat
+Given two strings, append them together (known as "concatenation") and return the result.
+However, if the strings are different lengths,omit chars from the longer string so it is the same length as the shorter string.
+So "Hello" and "Hi" yield "loHi". The strings may be any length.
+*/
+
+public String minCat(String a, String b) {
+    if (a.length() > b.length()) {
+        return a.substring(a.length() - b.length()) + b;
+    }
+    return a + b.substring(b.length() - a.length());
+}
