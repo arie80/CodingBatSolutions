959e3be17e24520a98888944d3f6284d2588fd81
Ari Sutanto <arisuttanto@gmail.com>
2019-02-03 11:23:06 +0700

    warmup-2 stringMatch

--- Java/warmup-2/warmup-2.java
+++ Java/warmup-2/warmup-2.java
@@ -120,3 +120,20 @@ public boolean array123(int[] nums) {
     }
     return false;
 }
+
+/*
+stringMatch
+Given 2 strings, a and b, return the number of the positions where they contain the same length 2 substring.
+So "xxcaazz" and "xxbaaz" yields 3, since the "xx", "aa", and "az" substrings appear in the same place in both strings.
+*/
+
+public int stringMatch(String a, String b) {
+    int count = 0;
+    int len = Math.min(a.length(), b.length());
+    for (int i = 0; i < len - 1; i++) {
+        String aSub = a.substring(i, i + 2);
+        String bSub = b.substring(i, i + 2);
+        if (aSub.equals(bSub)) count++;
+    }
+    return count;
+}
