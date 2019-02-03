05860897119bbdc4a2e5a55c0a648c73a5ba68e1
Ari Sutanto <arisuttanto@gmail.com>
2019-02-03 14:32:25 +0700

    array-1 biggerTwo

--- Java/array-1/array-1.java
+++ Java/array-1/array-1.java
@@ -190,7 +190,21 @@ public int start1(int[] a, int[] b) {
         if(a[i] == 1) countA++;
     }
     for (int x = 0; x < b.length; x++) {
-        if ( b[x] == 1) countB++;
+        if (b[x] == 1) countB++;
     }
     return countA + countB;
+}
+
+/*
+biggerTwo
+Start with 2 int arrays, a and b, each length 2. Consider the sum of the values in each array.
+Return the array which has the largest sum. In event of a tie, return a.
+*/
+
+public int[] biggerTwo(int[] a, int[] b) {
+    int sumA = a[0] + a[1];
+    int sumB = b[0] + b[1];
+
+    if (sumA > sumB || sumA == sumB) return a;
+    return b;
 }
