a1284641717f8586eae5b6da164a24ccd631575d
Ari Sutanto <arisuttanto@gmail.com>
2019-02-03 12:15:10 +0700

    string-1 nonStart

--- Java/string-1/string-1.java
+++ Java/string-1/string-1.java
@@ -93,3 +93,12 @@ public String comboString(String a, String b) {
     }
     return b + a + b;
 }
+
+/*
+nonStart
+Given 2 strings, return their concatenation, except omit the first char of each. The strings will be at least length 1.
+*/
+
+public String nonStart(String a, String b) {
+    return a.substring(1) + b.substring(1);
+}
