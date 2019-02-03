80cf1894c67b6da49a4142eeac5db63b7bd8cf4d
Ari Sutanto <arisuttanto@gmail.com>
2019-02-03 13:58:20 +0700

    array-1 sameFirstLast

--- Java/array-1/array-1.java
+++ Java/array-1/array-1.java
@@ -7,3 +7,12 @@ The array will be length 1 or more.
 public boolean firstLast6(int[] nums) {
     return nums[0] == 6 || nums[nums.length - 1] == 6;
 }
+
+/*
+sameFirstLast
+Given an array of ints, return true if the array is length 1 or more, and the first element and the last element are equal.
+*/
+
+public boolean sameFirstLast(int[] nums) {
+    return nums.length >= 1 && (nums[0] == nums[nums.length - 1]);
+}
