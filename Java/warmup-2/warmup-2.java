a4b84be2cd4e04c453e5dc4851ec3f3ce884d2e9
Ari Sutanto <arisuttanto@gmail.com>
2019-02-03 10:59:02 +0700

    warmup-2 stringBits

--- Java/warmup-2/warmup-2.java
+++ Java/warmup-2/warmup-2.java
@@ -56,3 +56,16 @@ boolean doubleX(String str) {
     return x.startsWith("xx");
     */
 }
+
+/*
+stringBits
+Given a string, return a new string made of every other char starting with the first, so "Hello" yields "Hlo".
+*/
+
+public String stringBits(String str) {
+    String result = "";
+    for (int i = 0; i < str.length(); i += 2) {
+        result += str.substring(i, i + 1);
+    }
+    return result;
+}
