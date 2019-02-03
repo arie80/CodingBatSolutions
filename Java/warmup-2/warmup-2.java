ed6d2a1cc312d9eae2acfd6e38daeb052fc5203f
Ari Sutanto <arisuttanto@gmail.com>
2019-02-03 11:03:48 +0700

    warmup-2 stringSplosion

--- Java/warmup-2/warmup-2.java
+++ Java/warmup-2/warmup-2.java
@@ -69,3 +69,16 @@ public String stringBits(String str) {
     }
     return result;
 }
+
+/*
+stringSplosion
+Given a non-empty string like "Code" return a string like "CCoCodCode".
+*/
+
+public String stringSplosion(String str) {
+    String result = "";
+    for (int i = 0; i < str.length(); i++) {
+        result += str.substring(0, i + 1 );
+    }
+    return result;
+}
