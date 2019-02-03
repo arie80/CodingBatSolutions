6e7cc7c58c685e119fb75ae3b0c78354b0bd401f
Ari Sutanto <arisuttanto@gmail.com>
2019-02-03 12:33:12 +0700

    string-1 hasBad

--- Java/string-1/string-1.java
+++ Java/string-1/string-1.java
@@ -206,3 +206,16 @@ public String middleThree(String str) {
     String b = str.substring(str.length() / 2);
     return a.substring(a.length() - 1) + b.substring(0, 2);
 }
+
+/*
+hasBad
+Given a string, return true if "bad" appears starting at index 0 or 1 in the string,
+such as with "badxxx" or "xbadxx" but not "xxbadxx". The string may be any length, including 0.
+Note: use .equals() to compare 2 strings.
+*/
+
+public boolean hasBad(String str) {
+    if (str.length() >= 3 && str.substring(0, 3).equals("bad") ||
+        (str.length() > 3 && str.substring(1, 4).equals("bad"))) return true;
+    return false;
+}
