936de64f7bb1b1fe01be7ceb556a11642d96a136
Ari Sutanto <arisuttanto@gmail.com>
2019-02-03 11:18:29 +0700

    warmup-2 array123

--- Java/warmup-2/warmup-2.java
+++ Java/warmup-2/warmup-2.java
@@ -108,3 +108,15 @@ public int arrayCount9(int[] nums) {
     }
     return count;
 }
+
+/*
+array123
+Given an array of ints, return true if the sequence of numbers 1, 2, 3 appears in the array somewhere.
+*/
+
+public boolean array123(int[] nums) {
+    for (int i = 0; i < nums.length -2; i++) {
+        if (nums[i] == 1 && nums[i + 1] == 2 && nums[i + 2] == 3) return true;
+    }
+    return false;
+}
