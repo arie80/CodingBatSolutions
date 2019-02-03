67268659f90b2efc9776953f639c5637bdb9f2d1
Ari Sutanto <arisuttanto@gmail.com>
2019-02-03 13:49:26 +0700

    string-1 withoutX

--- Java/string-1/string-1.java
+++ Java/string-1/string-1.java
@@ -382,3 +382,22 @@ public String startWord(String str, String word) {
     }
     return result;
 }
+
+/*
+withoutX
+Given a string, if the first or last chars are 'x', return the string without those 'x' chars,
+and otherwise return the string unchanged.
+*/
+
+public String withoutX(String str) {
+
+  if (str.length() > 0 && str.charAt(0) == 'x') {
+    str = str.substring(1);
+  }
+
+  if (str.length() > 0 && str.charAt(str.length() - 1) == 'x') {
+    str = str.substring(0, str.length() - 1);
+  }
+
+  return str;
+}
