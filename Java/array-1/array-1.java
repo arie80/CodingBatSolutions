d79c0b83770fd5f915db767f1cd65f97c4c5bc2b
Ari Sutanto <arisuttanto@gmail.com>
2019-02-03 14:19:22 +0700

    array-1 double23

--- Java/array-1/array-1.java
+++ Java/array-1/array-1.java
@@ -150,3 +150,13 @@ public int[] makeLast(int[] nums) {
     arr[arr.length -1] = nums[nums.length -1];
     return arr;
 }
+
+/*
+double23
+Given an int array, return true if the array contains 2 twice, or 3 twice. The array will be length 0, 1, or 2.
+*/
+
+public boolean double23(int[] nums) {
+    if (nums.length <= 1) return false;
+    return (nums[0] == 2 && nums[1] == 2) || (nums[0] == 3 && nums[1] == 3);
+}
