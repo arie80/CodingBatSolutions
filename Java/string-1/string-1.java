07608b5726d557e836362aa090ca862a2aa89676
Ari Sutanto <arisuttanto@gmail.com>
2019-02-03 13:31:41 +0700

    string-1 frontAgain

--- Java/string-1/string-1.java
+++ Java/string-1/string-1.java
@@ -284,3 +284,14 @@ public String seeColor(String str) {
     if (str.length() >= 4 && str.substring(0, 4).equals("blue")) result = "blue";
     return result;
 }
+
+/*
+frontAgain
+Given a string, return true if the first 2 chars in the string also appear at the end of the string, such as with "edited".
+*/
+
+public boolean frontAgain(String str) {
+    if (str.length() < 2) return false;
+    String front = str.substring(0, 2);
+    return front.equals(str.substring(str.length() - 2));
+}
