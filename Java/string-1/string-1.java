65f302034b3eeac1ce8d6476ba25e5768c59a948
Ari Sutanto <arisuttanto@gmail.com>
2019-02-03 12:30:43 +0700

    string-1 twoChar

--- Java/string-1/string-1.java
+++ Java/string-1/string-1.java
@@ -180,3 +180,17 @@ The string length will be at least n.
 public String nTwice(String str, int n) {
     return str.substring(0, n) + str.substring(str.length() - n);
 }
+
+/*
+twoChar
+Given a string and an index, return a string length 2 starting at the given index.
+If the index is too big or too small to define a string length 2, use the first 2 chars.
+The string length will be at least 2.
+*/
+
+public String twoChar(String str, int index) {
+    if (index < 1 || index > str.length() - 2) {
+        return str.substring(0, 2);
+    }
+    return str.substring(index, index + 2);
+}
