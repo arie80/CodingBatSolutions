481d69cd281462fc99bd86cd9eeb398575ac7f82
Ari Sutanto <arisuttanto@gmail.com>
2019-02-03 12:14:03 +0700

    string-1 comboString

--- Java/string-1/string-1.java
+++ Java/string-1/string-1.java
@@ -79,3 +79,17 @@ The string length will be at least 2.
 public String withoutEnd(String str) {
     return str.substring(1, str.length() - 1);
 }
+
+/*
+comboString
+Given 2 strings, a and b, return a string of the form short+long+short,
+with the shorter string on the outside and the longer string on the inside.
+The strings will not be the same length, but they may be empty (length 0).
+*/
+
+public String comboString(String a, String b) {
+    if (a.length() < b.length()) {
+        return a + b + a;
+    }
+    return b + a + b;
+}
