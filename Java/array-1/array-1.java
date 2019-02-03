b669fe3bb171582ca571c1278a575e800fb23dd6
Ari Sutanto <arisuttanto@gmail.com>
2019-02-03 14:02:46 +0700

    array-1 sum3

--- Java/array-1/array-1.java
+++ Java/array-1/array-1.java
@@ -36,3 +36,12 @@ Both arrays will be length 1 or more.
 public boolean commonEnd(int[] a, int[] b) {
     return a[0] == b[0] || a[a.length - 1] == b[b.length - 1];
 }
+
+/*
+sum3
+Given an array of ints length 3, return the sum of all the elements.
+*/
+
+public int sum3(int[] nums) {
+    return nums[0] + nums[1] + nums[2];
+}
