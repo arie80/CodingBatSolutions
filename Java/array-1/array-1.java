fae9b6761b8f6523df612b173684b29c56a95efc
Ari Sutanto <arisuttanto@gmail.com>
2019-02-03 14:16:17 +0700

    array-1 no23

--- Java/array-1/array-1.java
+++ Java/array-1/array-1.java
@@ -128,3 +128,12 @@ Given an int array length 2, return true if it contains a 2 or a 3.
 public boolean has23(int[] nums) {
     return (nums[0] == 2 || nums[1] == 2) || (nums[0] == 3 || nums[1] == 3);
 }
+
+/*
+no23
+Given an int array length 2, return true if it does not contain a 2 or 3.
+*/
+
+public boolean no23(int[] nums) {
+    return (nums[0] != 2 && nums[1] != 2) && (nums[0] != 3 && nums[1] != 3);
+}
