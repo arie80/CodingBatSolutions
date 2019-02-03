759d0f32e1aa9a244e74054e0ba4e71b665e281f
Ari Sutanto <arisuttanto@gmail.com>
2019-02-03 11:35:36 +0700

    warmup-2 stringYak

--- Java/warmup-2/warmup-2.java
+++ Java/warmup-2/warmup-2.java
@@ -170,3 +170,21 @@ public String altPairs(String str) {
     }
     return result;
 }
+
+/*
+stringYak
+Suppose the string "yak" is unlucky. Given a string, return a version where all the "yak" are removed,
+but the "a" can be any char. The "yak" strings will not overlap.
+*/
+
+public String stringYak(String str) {
+    String result = "";
+    for (int i = 0; i < str.length(); i++) {
+        if (i + 2 < str.length() && str.charAt(i) == 'y' && str.charAt(i + 2) == 'k') {
+            i = i + 2;
+        } else {
+            result += str.charAt(i);
+        }
+    }
+    return result;
+}
