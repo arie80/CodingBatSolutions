3a597e4fa01257fd671fb0fb809bb91b5a071a44
Ari Sutanto <arisuttanto@gmail.com>
2019-02-03 14:34:03 +0700

    array-1 makeMiddle

--- Java/array-1/array-1.java
+++ Java/array-1/array-1.java
@@ -208,3 +208,15 @@ public int[] biggerTwo(int[] a, int[] b) {
     if (sumA > sumB || sumA == sumB) return a;
     return b;
 }
+
+/*
+makeMiddle
+Given an array of ints of even length, return a new array length 2 containing the middle two elements from the original array.
+The original array will be length 2 or more.
+*/
+
+public int[] makeMiddle(int[] nums) {
+    int divide = nums.length / 2;
+    int[] mid = {nums[divide - 1], nums[divide]};
+    return mid;
+}
