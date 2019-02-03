86a06268e6c9319a5e2f4ef9bf9301e9f65adb9a
Ari Sutanto <arisuttanto@gmail.com>
2019-02-03 13:19:06 +0700

    string-1 lastChars

--- Java/string-1/string-1.java
+++ Java/string-1/string-1.java
@@ -231,3 +231,15 @@ public String atFirst(String str) {
     if (str.length() < 2) return str + "@";
     return str.substring(0, 2);
 }
+
+/*
+lastChars
+Given 2 strings, a and b, return a new string made of the first char of a and the last char of b,
+so "yo" and "java" yields "ya". If either string is length 0, use '@' for its missing char.
+*/
+
+public String lastChars(String a, String b) {
+    if (a.length() == 0) a = "@";
+    if (b.length() == 0) b = "@";
+    return a.substring(0, 1) + b.substring(b.length() -1);
+}
