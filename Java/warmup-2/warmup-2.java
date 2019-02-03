5e7e6ea0dae059f0e7a747e5ed1d78d280ae8f4f
Ari Sutanto <arisuttanto@gmail.com>
2019-02-03 11:39:31 +0700

    warmup-2 noTriples

--- Java/warmup-2/warmup-2.java
+++ Java/warmup-2/warmup-2.java
@@ -204,3 +204,17 @@ public int array667(int[] nums) {
     }
     return count;
 }
+
+/*
+noTriples
+Given an array of ints, we'll say that a triple is a value appearing 3 times in a row in the array.
+Return true if the array does not contain any triples.
+*/
+
+public boolean noTriples(int[] nums) {
+    for (int i = 0; i < nums.length - 2; i++) {
+        int first= nums[i];
+        if (nums[i + 1] == first && nums[i + 2] == first) return false;
+    }
+    return true;
+}
