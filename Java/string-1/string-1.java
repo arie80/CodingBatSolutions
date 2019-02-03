48c8dc5db5597cbbb1a0e0324cf4447e6033655e
Ari Sutanto <arisuttanto@gmail.com>
2019-02-03 12:25:00 +0700

    string-1 middleTwo

--- Java/string-1/string-1.java
+++ Java/string-1/string-1.java
@@ -147,3 +147,16 @@ public String withouEnd2(String str) {
     }
     return result;
 }
+
+/*
+middleTwo
+Given a string of even length, return a string made of the middle two chars, so the string "string" yields "ri".
+The string length will be at least 2.
+*/
+
+public String middleTwo(String str) {
+    int split = str.length() / 2;
+    String a = str.substring(0, split);
+    String b = str.substring(split, str.length());
+    return a.substring(a.length() - 1) + b.substring(0, 1);
+}
