9b1fa739204c67e1970de3c1d29d5531411a926b
Ari Sutanto <arisuttanto@gmail.com>
2019-02-06 14:38:57 +0700

    cleaning up white space

--- Java/logic-2/logic-2.java
+++ Java/logic-2/logic-2.java
@@ -38,11 +38,8 @@ So for example, if b is 13, then both b and c do not count.
 public int luckySum(int a, int b, int c) {
     int sum = 0;
     if (a == 13) return sum;
-    
     if (a != 13) sum += a;
-    
     if (b != 13) sum += b;
-    
     if (b != 13 && c != 13) sum += c;
     return sum;
 }
